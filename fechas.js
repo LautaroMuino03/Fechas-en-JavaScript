b8321af5498b20a5fe9a09f9ca72445687f0122b
Lautaro Muiño <117776336+LautaroMuino03@users.noreply.github.com>
2023-02-27 12:06:19 -0300

    Add files via upload

---
+++ fechas.js
@@ -0,0 +1,11 @@
+const fecha = new Date()
+console.log(fecha)
+
+const fecha2 = new Date(2003, 1, 23)
+console.log(fecha2);
+
+console.log(fecha > fecha2);
+
+console.log(fecha2.getDate());
+console.log(fecha2.getMonth());
+console.log(fecha2.getFullYear());
